3fa79b3a513972e1021df77b321eebdfe97df4f9
Ayush28o <ayushraj280503@gmail.com>
2026-02-14 21:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function playMusic() {
+    document.getElementById("music").innerHTML =
+    `<audio autoplay loop>
+       <source src="https://cdn.pixabay.com/audio/2022/03/15/audio_3a9e4e2b75.mp3" type="audio/mp3">
+     </audio>`;
+}
